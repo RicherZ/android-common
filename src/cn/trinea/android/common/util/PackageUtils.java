c3a7f3a6ba52580b17fe3a265814d41b7186cf58
RicherZ <23746021@163.com>
2015-08-17 18:08:59 +0800

    add getAppVersionName method

--- src/cn/trinea/android/common/util/PackageUtils.java
+++ src/cn/trinea/android/common/util/PackageUtils.java
@@ -478,6 +478,30 @@ public class PackageUtils {
     }
 
     /**
+     * get app version name
+     * 
+     * @param context
+     * @return
+     */
+    public static String getAppVersionName(Context context) {
+        if (context != null) {
+            PackageManager pm = context.getPackageManager();
+            if (pm != null) {
+                PackageInfo pi;
+                try {
+                    pi = pm.getPackageInfo(context.getPackageName(), 0);
+                    if (pi != null) {
+                        return pi.versionName;
+                    }
+                } catch (NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "";
+    }
+
+    /**
      * get system install location<br/>
      * can be set by System Menu Setting->Storage->Prefered install location
      * 
